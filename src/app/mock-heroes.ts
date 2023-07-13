ecf941ab9448fc11ddc8b2f24af855fc8800e06d
SUDHAKAR1202 <sudzcrazy94@gmail.com>
2023-07-13 18:11:07 +0530

    tour of heroes app

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id:1, name:'Pawan kalyan'},
+  { id: 2,  name: 'Mahesh Babu' },
+  { id: 3, name: 'Jr NTR' },
+  { id: 4, name: 'Ram Charan' },
+  { id: 5, name: 'Chiranjeevi' },
+  { id: 6, name: 'Allu Arjun' },
+  { id: 7, name: 'Nagarjuna' },
+  { id: 8, name: 'Venkatesh' },
+  { id: 9, name: 'Balayya' },
+  { id: 10, name: 'Sai dharam tej' }
+];
